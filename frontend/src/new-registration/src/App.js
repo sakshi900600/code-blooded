7a2200162dbca1ba21cd0eb8c2988f2dd2243b59
suryansh-cpu <lemononator23@gmail.com>
2025-08-02 13:52:26 +0530

    Moved NewRegistration files into new-registration folder

---
+++ frontend/src/new-registration/src/App.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// // import Register from "./pages/Register.jsx";
+// import Register from "./pages/Register.jsx";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/register" element={<Register />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import React from 'react';
+import Register from './Register.jsx';
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
